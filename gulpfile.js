9fbddc19b639a65174a140fec4a438918cb8c3f4
Darwin Campa <yorb@users.noreply.github.com>
2017-05-23 17:08:36 -0400

    Improve build flow and move vendor assets into subfolder

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,56 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var pump = require('pump');
 
-gulp.task('sass', () =>
-  gulp.src('scss/main.scss')
-    .pipe(sass({
-        includePaths: [
-          'bower_components/foundation-sites/scss'
-        ]
-      }).on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3']
-      }))
-    .pipe(gulp.dest('_site/assets/css'))
-);
-
-gulp.task('js', () =>
-  gulp.src([
-      'bower_components/jquery/dist/jquery.min.js',
-      'bower_components/what-input/dist/what-input.min.js',
-      'bower_components/foundation-sites/dist/js/foundation.min.js'
-    ])
-    .pipe(gulp.dest('_site/assets/js'))
-);
-
-gulp.task('watch', ['sass', `js`], () => {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+gulp.task('sass', (cb) => {
+  pump([
+      gulp.src('scss/main.scss'),
+      sourcemaps.init(),
+        sass({includePaths: ['bower_components/foundation-sites/scss']}).on('error', sass.logError),
+        autoprefixer({browsers: ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3']}),
+        cleanCSS(),
+      sourcemaps.write(),
+      gulp.dest('assets/css')
+    ],
+    cb
+  );
 });
 
-gulp.task('default', ['sass', 'js']);
+gulp.task('vendor-js', (cb) => {
+  pump([
+      gulp.src([
+          'bower_components/jquery/dist/jquery.min.js',
+          'bower_components/what-input/dist/what-input.min.js',
+          'bower_components/foundation-sites/dist/js/foundation.min.js',
+          'bower_components/svg-injector/dist/svg-injector.min.js'
+        ]),
+      gulp.dest('assets/js/vendor')
+    ],
+    cb
+  );
+});
+
+gulp.task('js', (cb) => {
+  pump([
+      gulp.src('js/*.js'),
+      sourcemaps.init(),
+        uglify(),
+        rename({suffix: '.min'}),
+      sourcemaps.write(),
+      gulp.dest('assets/js')
+    ],
+    cb
+  );
+});
+
+gulp.task('watch', () => {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch(['js/*.js'], ['js']);
+});
+
+gulp.task('default', ['sass', 'vendor-js', 'js']);
