87af8f2da084f1d6bd8b3840c7d425970fc6d68b
Darwin Campa <yorb@users.noreply.github.com>
2017-08-16 15:59:10 -0400

    Fix some screen size bugs, reorg JS to handle size changes after initial load

--- js/home.js
+++ js/home.js
@@ -1,41 +1,73 @@
 $(document).ready(function() {
   'use strict';
 
+  // Initialize all the things
+  setPanelHeight(Foundation.MediaQuery.current);
+  initMoreArrow(Foundation.MediaQuery.current, null);
+  addDetailTransitions(Foundation.MediaQuery.current, null);
+
+  // Re-run size-specific things on window resize
+  $(window).resize(function() {
+    setPanelHeight(Foundation.MediaQuery.current);
+  });
+
+  // Re-run some things when the size class changes
+  $(window).on('changed.zf.mediaquery', function(e, newSize, oldSize) {
+
+    initMoreArrow(newSize, oldSize);
+    addDetailTransitions(newSize, oldSize);
+  });
 
   // Set the panel height on mobile screens to account for collapsing browser chrome
-  if (Foundation.MediaQuery.current == 'small') {
-    var heightOffset = viewport.max - viewport.min;
-    var navHeight = $('.nav-container').height();
+  function setPanelHeight(size) {
+    if (size === 'small') {
+      var heightOffset = viewport.max - viewport.min;
+      var navHeight = $('.nav-container').height();
 
-    $('#hero').css('height', viewport.min - navHeight);
-    $('article').css('height', viewport.max);
+      $('#hero').css('height', viewport.min - navHeight);
+      $('article').css('height', viewport.max);
+    } else {
+      $('#hero').css('height', '');
+      $('article').css('height', '');
+    }
   }
 
-  // init more arrow
-  var options = {
-   barOffset: Foundation.MediaQuery.current == 'small' ? -25 : 55,
-   animationDuration: 300,
-   animationEasing: 'swing'
-  };
-  var moreArrow = new Foundation.Magellan($('.more-arrow'), options);
+  // Add smooth scrolling to the "more" arrow
+  function initMoreArrow(newSize, oldSize) {
+    if (newSize === 'small' || oldSize === 'small' || oldSize === null) {
+      var options = {
+       barOffset: newSize === 'small' ? -25 : 55,
+       animationDuration: 300,
+       animationEasing: 'swing'
+      };
 
-  // ease transition to product detail
-  if (Foundation.MediaQuery.atLeast('medium')) {
-    $('article.product').each(function() {
-      var $product = $(this);
-      var $links = $('[href^="/products"]', $product);
+      var moreArrow = new Foundation.Magellan($('.more-arrow'), options);
+    }
+  }
 
-      $links.each(function() {
-        $(this).click(function(e) {
-          e.preventDefault();
-          $product.addClass('in-transition');
-          $('.pager ul').foundation('scrollToLoc', '#' + $product.attr('id'));
+  // Ease transition to product detail
+  function addDetailTransitions(newSize, oldSize) {
+    if (Foundation.MediaQuery.atLeast('medium') && (oldSize === null || oldSize === 'small')) {
+      $('article.product').each(function() {
+        var $product = $(this);
+        var $links = $('[href^="/products"]', $product);
 
-          var navigate = setTimeout(function(dest) {
-            location.href = dest;
-          }, 300, e.target.href);
+        $links.each(function() {
+          $(this).click(function(e) {
+            e.preventDefault();
+            $product.addClass('in-transition');
+            $('.pager ul').foundation('scrollToLoc', '#' + $product.attr('id'));
+
+            var navigate = setTimeout(function(dest) {
+              location.href = dest;
+            }, 300, e.target.href);
+          });
         });
       });
-    });
+    } else if (newSize === 'small') {
+      $('article.product [href^="/products"]').each(function() {
+        $(this).off('click');
+      });
+    }
   }
 });
